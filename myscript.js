414bcf9408f844fd6ed1c95047f6e91991be5195
khushi-shah701 <148178865+khushi-shah701@users.noreply.github.com>
2023-10-17 10:32:12 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,16 +1,3 @@
-<html>
-<body>
-
-<h2>External JavaScript</h2>
-
-<p id="demo">A Paragraph.</p>
-
-<button type="button" onclick="myFunction()">Click Me</button>
-
-<p>This example uses a full web URL to link to "myScript.js".</p>
-<p>(myFunction is stored in "myScript.js")</p>
-
-<script src="./myScript.js"></script>
-
-</body>
-</html>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
